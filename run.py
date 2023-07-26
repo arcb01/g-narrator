00879e4039adb6f9b697a1f392b5f49a4eaf0cef
arnaucasbas <arcascb2001@gmail.com>
2023-07-26 13:57:03 +0200

    added CLI arguments

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
 
 from modules.app import App, settings
+import argparse
 
 if __name__ == '__main__':
-    # 1. Settings
-    lang = "en"
-    gpu = True
-    voice_speed = 150
-    # 2. Run App
-    tts, ocr = settings(lang, gpu, voice_speed)
+    # Arguments for CLI
+    parser = argparse.ArgumentParser(description="Gaming Narrator")
+    parser.add_argument("--lang", type=str, help="Language for TTS", default="en")
+    parser.add_argument("--gpu", type=bool, help="Use GPU for OCR", default=True)
+    parser.add_argument("--voice_speed", type=int, help="Voice speed for TTS", default=150)
+    args = parser.parse_args()
+
+    tts, ocr = settings(args.lang, args.gpu, args.voice_speed)
     a = App(tts, ocr)
     a.run() 
