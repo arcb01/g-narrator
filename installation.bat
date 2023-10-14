695c4ee95cf2f292c963079eccb457dbb8b65063
arnaucasbas <arcascb2001@gmail.com>
2023-10-14 21:39:58 +0200

    new bat

--- installation.bat
+++ installation.bat
@@ -1,27 +1,18 @@
 @echo off
 
-rem Set the name of the virtual environment
-set VENV_NAME=.env
+rem Check if the virtual environment directory exists, if not, create it
+if not exist .env (
+	echo Creating environment. Please wait...
+    python -m venv .env
+    call .env\Scripts\activate
 
-rem Check if the virtual environment already exists
-if exist %VENV_NAME% (
-    echo Starting...
-    goto :run_script
+    rem Install or upgrade dependencies (modify this line as needed)
+    python setup.py install
+) else (
+    rem Activate the virtual environment
+    call .env\Scripts\activate
 )
 
-rem Set the path to your Python executable (change this if necessary)
-set PYTHON_EXECUTABLE=python
-
-rem Create the virtual environment (suppress output)
-%PYTHON_EXECUTABLE% -m venv %VENV_NAME% > NUL
-%PYTHON_EXECUTABLE% -m pip config set global.disable-pip-version-check true
-
-rem Activate the virtual environment
-call %VENV_NAME%\Scripts\activate.ps1
-
-rem Install dependencies from the requirements file (suppress output)
-python setup.py install > NUL
-
-:run_script
-rem Run your Python script
+echo Starting. Please wait...
+rem Run the Python script
 python .\garrator\run.py
