bb518446214c6dc6eb8c324c05c174606f6c1286
arnaucasbas <arcascb2001@gmail.com>
2023-10-15 11:53:59 +0200

    og

--- installation.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-rem Check if the virtual environment directory exists, if not, create it
-echo Creating environment. Please wait...
-python -m venv .env
-call .env\Scripts\activate
-
-rem Install or upgrade dependencies (modify this line as needed)
-python setup.py install
