e72b85c316f56202b133670b7c2bb433bc9a0261
arnaucasbas <arcascb2001@gmail.com>
2023-11-02 16:16:35 +0100

    new installation script

--- installation.bat
+++ installation.bat
@@ -7,3 +7,18 @@ call .env\Scripts\activate
 
 rem Install or upgrade dependencies (modify this line as needed)
 python setup.py install
+
+rem Check the exit code of the installation process
+if %errorlevel% equ 0 (
+    echo -----------------------------------------
+    echo Installation was successful.
+    echo You can now close this window.
+    echo -----------------------------------------
+) else (
+    echo -----------------------------------------
+    echo Installation failed. Please check for errors and try again.
+    echo -----------------------------------------
+)
+
+rem Pause to keep the window open until the user presses a key
+pause
