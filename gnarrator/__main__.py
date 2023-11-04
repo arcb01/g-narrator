33b8c0138a4e8e93901f20f29689b30bff00815d
arnaucasbas <arcascb2001@gmail.com>
2023-11-04 12:39:25 +0100

    adding cli support

--- gnarrator/__main__.py
+++ gnarrator/__main__.py
@@ -1,14 +1,25 @@
 from gnarrator.app import App
+import click
 
-def _main():
-    # Settings for TTS and OCR
-    settings = {"LANGUAGE": "en", 
-                "VOICE_RATE": "+5%",
-                "VOICE_VOLUME": "+0%",
-                "GENDER": "male"}
-    
+
+@click.command()
+@click.option('--language', default="en", help="Language setting")
+@click.option('--voice-rate', default="+0%", help="Voice rate setting")
+@click.option('--voice-volume', default="+0%", help="Voice volume setting")
+@click.option('--gender', default="male", help="Gender setting")
+def _main(language, voice_rate, voice_volume, gender):
+
+    settings = {
+                "LANGUAGE": language,
+                "VOICE_RATE": voice_rate,
+                "VOICE_VOLUME": voice_volume,
+                "GENDER": gender
+                }
+
+    # Start the app
     gnarrator = App(settings)
     gnarrator.run()
+
 
 if __name__ == '__main__':
     _main()
