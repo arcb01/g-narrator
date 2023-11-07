8c5c24896a99c6ee7bd1bca593faa214d171b67f
arnaucasbas <arcascb2001@gmail.com>
2023-11-07 21:35:55 +0100

    v0.9.3

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ for e in ["utf-8", "utf-8-sig", "utf16"]:
 
 setuptools.setup(
     name='gnarrator',
-    version='0.9.121',
+    version='0.9.3',
     python_requires='>=3.9.0',
     author='Arnau Castelalno',
     author_email='arcascb2001@gmail.com',
